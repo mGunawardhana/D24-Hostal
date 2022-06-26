4ed878a48e0a38bb02df7a898585d4f69bd56eeb
mGunawardhana <mrgunawardhana27368@gmail.com>
2022-06-26 22:32:06 +0530

    added lombok annotaions for User

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,7 +1,16 @@
 package entity;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
 
 @Entity
 public class User implements SuperEntity{
